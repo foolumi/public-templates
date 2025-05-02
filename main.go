e19a85d0fce4737af8daf7934f276e9a72ff5b08
Derek <dschaller@users.noreply.github.com>
2025-05-01 20:14:46 -0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+  
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+  "github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		config := config.New(ctx, "")
+    phrase := fmt.Sprintf("Hello %s", config.Get("name"))
+    fmt.Println(phrase)
+
+		ctx.Export("phrase", pulumi.String(phrase))
+		return nil
+	})
+}
